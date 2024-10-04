cb282c92eb157f1e6b281d087de990603643c955
mrym.86 <na72tsu86@gmail.com>
2024-10-04 21:17:57 +0900

    アカウント登録画面の作成

--- resources/views/auth/custom_register.blade.php
+++ resources/views/auth/custom_register.blade.php
@@ -10,10 +10,42 @@
 <body>
   <div class="register_container">
     <div class="container">
-      <h1>ユーザー登録</h1>
-        <input text="name" name = "name">
-        <input text="email" name = "email">
-        <input checkbox name="admin">
-        <input text="password">
+      <h2>ユーザー登録</h2>
+        <form method="POST" action="{{ route('register')}}">
+          @csrf
+            <div class="form-group">
+              <label for="name" value="__('Name')">名前</label>
+              @error('name')
+              <span style ="color:red">入力内容が正しくありません</span>
+              @enderror
+              <input type="name" name="name" id="name" value="{{old('name')}}" required>
+            </div>
+            <div class="form-group">
+              <label for="email" value="__('Email')">メールアドレス</label>
+              @error('name')
+              <span style ="color:red">入力内容が正しくありません</span>
+              @enderror
+              <input type="email" name="email" id="email" value="{{old('email')}}" required>
+            </div>
 
-  </div>
+            <div class="form-group">
+              <label for="admin" value="__('Admin')">あなたは管理者ですか？</label>
+              
+              <input type="checkbox" name="admin" id="admin" value="{{old('admin')}}" >
+            </div>
+
+            <div class="form-group">
+              <label for="password" value="__('password')">パスワード</label>
+              @error('name')
+              <span style ="color:red">入力内容が正しくありません</span>
+              @enderror
+              <input type="password" name="password" id="password" value="{{old('password')}}" required>
+            </div>
+
+            <div class="register_btn">
+              <button type="submit">{{__('新規登録')}}</button>
+            </div>
+        </form>
+    </div>
+  </div>  
+</body>   
